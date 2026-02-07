a400cca562fc5020e341d438ce26d26ca787747b
khairnarmohit <102132224+khairnarmohit@users.noreply.github.com>
2026-02-07 05:53:29 -0800

    Update app.js

--- My-Jenkins/app.js
+++ My-Jenkins/app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => {
 
-  res.send("This Jenkins(CI/CD) Nodejs Project Mohit bhai  🔥");
+  res.send("This Jenkins(CI/CD) Nodejs Project chetan aapa 🔥");
 });
 
 app.listen(3000, "0.0.0.0", () => {
