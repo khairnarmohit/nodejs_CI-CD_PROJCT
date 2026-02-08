2289885da5b45252253a8a78b17f076e32ae7025
khairnarmohit <102132224+khairnarmohit@users.noreply.github.com>
2026-02-08 04:37:34 -0800

    Update app.js

--- My-Jenkins/app.js
+++ My-Jenkins/app.js
@@ -1,4 +1,4 @@
-const express = require("");
+const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   res.send("This Jenkins(CI/CD) Nodejs Project Mohit Devops ENG send email 🔥");
 });
 
-app.listen(3000, "0.0.0.0", () => {
+app.listen(, "0.0.0.0", () => {
   console.log("Running on port 3000");
 });
 
